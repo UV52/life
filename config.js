2f60452d1da18b0345e05211bb03ad365b5e57ea
UV52 <ce1240861@iitd.ac.in>
2025-02-13 13:27:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -79,7 +79,7 @@ const CONFIG = {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "Title Track Shiddat 320 Kbps.mp3", // Music streaming URL
-        startText: "🎵 Play Music",        // Button text to start music
+        startText: "🎵 Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
     }
